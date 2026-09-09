948c4f0f8595c4616b9aff3f2fc1316310245295
Ishank <mishank258@gmail.com>
2026-09-09 19:56:28 +1000

    Add wp-config DB settings

---
+++ wp-config-db-section.php
@@ -0,0 +1,4 @@
+define( 'DB_NAME', 'wordpress_db' );
+define( 'DB_USER', 'admin' );
+define( 'DB_PASSWORD', 'WordPress123!' );
+define( 'DB_HOST', 'swe40006-wordpress-db.cne4wqysy391.ap-southeast-2.rds.amazonaws.com' );
